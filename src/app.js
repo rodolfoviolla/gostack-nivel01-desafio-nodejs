d7dd6ad117c38389f5d61ef5c5b5b5c0f7e2c4c9
Rodolfo Della Violla <rodolfo.violla@gmail.com>
2020-06-16 13:19:51 -0300

    Desafio realizado

--- src/app.js
+++ src/app.js
@@ -1,33 +1,76 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
+const { uuid } = require("uuidv4")
 
-// const { uuid } = require("uuidv4");
+const app = express()
 
-const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.use(express.json());
-app.use(cors());
-
-const repositories = [];
+const repositories = []
 
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  return response.json(repositories)
+})
 
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body
+  
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
+})
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  } else {
+    if (title) repositories[repositoryIndex].title = title
+    if (url) repositories[repositoryIndex].url = url
+    if (techs) repositories[repositoryIndex].techs = techs
+
+    return response.json(repositories[repositoryIndex])
+  }
+})
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  } else {
+    repositories.splice(repositoryIndex, 1)
+    
+    return response.status(204).send()
+  }
+})
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
 
-module.exports = app;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' })
+  } else {
+    repositories[repositoryIndex].likes++
+    return response.send(repositories[repositoryIndex])
+  }
+})
+
+module.exports = app
